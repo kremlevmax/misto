0e74818eacc3f79c29720bbc62b154a68fc42750
kremlevmax <kremlevmax.webdev@icloud.com>
2023-03-13 23:32:49 -0700

    first commit

---
+++ src/main/java/com/maxfromeverett/misto/MistoApplication.java
@@ -0,0 +1,13 @@
+package com.maxfromeverett.misto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MistoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MistoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxfromeverett/misto/MistoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxfromeverett.misto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MistoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
