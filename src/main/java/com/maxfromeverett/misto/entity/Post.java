eea95aa6b77e2ec72037b8c10d0d69cc1b2dc703
kremlevmax <kremlevmax.webdev@icloud.com>
2023-04-21 23:41:45 -0700

    Some experiments

--- src/main/java/com/maxfromeverett/misto/entity/Post.java
+++ src/main/java/com/maxfromeverett/misto/entity/Post.java
@@ -1,21 +1,16 @@
 package com.maxfromeverett.misto.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @NoArgsConstructor( force=true)
@@ -32,7 +27,7 @@ public abstract class Post {
   @NotBlank(message = "Description has to be provided for creating a post")
   private String description;
 
-  private List<MultipartFile> images;
+  private List<String> images;
 
   private String author;
   private String phoneNumber;
