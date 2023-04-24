f67bac5d9a1c19a231fff483e7f0c4ffb8c81f89
kremlevmax <kremlevmax.webdev@icloud.com>
2023-04-23 23:28:35 -0700

    Add File Upload Properties

--- src/main/java/com/maxfromeverett/misto/MistoApplication.java
+++ src/main/java/com/maxfromeverett/misto/MistoApplication.java
@@ -1,9 +1,14 @@
 package com.maxfromeverett.misto;
 
+import com.maxfromeverett.misto.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class MistoApplication {
 
 	public static void main(String[] args) {
