5783ca5484cb3a15e4d0b39c03b2aa48d4030d81
kremlevmax <kremlevmax.webdev@icloud.com>
2023-05-03 23:17:45 -0700

    Refactoring

--- src/main/java/com/maxfromeverett/misto/controller/ImageController.java
+++ src/main/java/com/maxfromeverett/misto/controller/ImageController.java
@@ -1,6 +1,11 @@
 package com.maxfromeverett.misto.controller;
 
+import com.maxfromeverett.misto.dto.SellPostDto;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RequestMapping(path = "/api/v1/images")
 public class ImageController {
-
+  @GetMapping("/{id}")
+  public ResponseEntity<SellPostDto> getSellPostById(@PathVariable Long id) {
+    return new ResponseEntity<SellPostDto>(SellPostDto.fromSellPost(sellPostService.getPostById(id)),
+        HttpStatus.OK);
+  }
 }
