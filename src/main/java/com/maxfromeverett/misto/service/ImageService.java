991a7652836060ae2f813a1b4b976122b0997255
kremlevmax <kremlevmax.webdev@icloud.com>
2023-05-06 23:54:16 -0700

    Refactoring

--- src/main/java/com/maxfromeverett/misto/service/ImageService.java
+++ src/main/java/com/maxfromeverett/misto/service/ImageService.java
@@ -3,7 +3,7 @@ package com.maxfromeverett.misto.service;
 import com.maxfromeverett.misto.entity.Image;
 
 public class ImageService {
-
+//
   public static Image getImageById(Long id) {
     return null;
   }
